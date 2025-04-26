fe09d4d1e60ce82710ec4098a66f0a517b6c57d2
Md Nahidul Islam <183149325+wnzid@users.noreply.github.com>
2025-04-26 10:47:45 +0300

    v4

---
+++ XMLReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+
+public class XMLReader
+{
+    public static void ReadXML(string path)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load(path);
+
+        foreach (XmlNode node in doc.DocumentElement)
+        {
+            string name = node["Name"]?.InnerText;
+            string age = node["Age"]?.InnerText;
+
+            Console.WriteLine($"[XML] Name: {name}, Age: {age}");
+        }
+    }
+}
